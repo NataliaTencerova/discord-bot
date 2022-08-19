c57e132ccdb28ab7fe9129baeab34ec72e466ad7
NataliaTencerova <natalia@tencerova.cz>
2022-08-19 17:21:12 +0200

    changing names of functions

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os, json, time
 
 startTime = time.time()
 
-def getLastMessage():
+def get_last_message():
     cmdGetLastMessage = 'curl -s -H "Authorization: Bot OTg2NTg3NDkyNDY5ODUwMTcy.GbPs8p.OJiou_Jo0o1_WuqVt8-bIDg_qwemSpoJTJ-GMM" -H "User-Agent: discordBot (https://discord.com/api/webhooks/986616508182560798/hYkOCfWeybUkr7ryT5uZXn2r3NlEg8AWzEqO6za0-dmE84IaBWGPTThSJ4cpD9mS1BJh, v0.4)" -H "Content-Type: application/json" -X GET https://discord.com/api/channels/986631988859510865/messages?after=988358852531597352'
     result = os.popen(cmdGetLastMessage).read()
     jsonData = json.loads(result)
@@ -17,19 +17,19 @@ def getLastMessage():
 
     return lastMessage
 
-def postMessage(messageContent):
+def post_message(messageContent):
         cmdPost = 'curl -H "Authorization: Bot OTg2NTg3NDkyNDY5ODUwMTcy.GbPs8p.OJiou_Jo0o1_WuqVt8-bIDg_qwemSpoJTJ-GMM" -H "User-Agent: discordBot (https://discord.com/api/webhooks/986616508182560798/hYkOCfWeybUkr7ryT5uZXn2r3NlEg8AWzEqO6za0-dmE84IaBWGPTThSJ4cpD9mS1BJh, v0.4)" -H "Content-Type: application/json" -X POST -d \'{"content":"' +messageContent+ '"}\' https://discord.com/api/channels/986631988859510865/messages'
         os.system(cmdPost)
 
 def reply_hi():
-    lastMessage = getLastMessage()
+    lastMessage = get_last_message()
     messageContent = 'Ahoj'
 
     if 'ahoj' in lastMessage['content'].lower() and lastMessage['name'] != 'Bot 297':
-        postMessage(messageContent)
+        post_message(messageContent)
 
 def check_weather():
-    lastMessage = getLastMessage()
+    lastMessage = get_last_message()
 
     if '!Weather' in lastMessage['content']:
         cmdGetWeather = 'curl -s "https://api.openweathermap.org/data/2.5/weather?appid=9ba642bc8b1825cca70ff6bd1066bcf4&lat=49.1533&lon=16.8765&units=metric" -H "Accept: application/json"'
@@ -47,7 +47,7 @@ def check_weather():
         strFeelsLike = str(feelsLike)
 
         messageContent = 'The weather in ' +strLocation+ ' is ' +strWeather+ '. The temperature is ' +strTemperature+ ', but it feels like ' +strFeelsLike+ '.'
-        postMessage(messageContent)
+        post_message(messageContent)
 
 
 while True:
